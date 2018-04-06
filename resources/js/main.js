eeb6d5ba2329b48dc3d6a13962b82886c511f168
Bezlonir <michael_bragg99@hotmail.com>
2018-04-05 20:13:16 -0600

    Fix draggable carousel.

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     accessibility:false,
     autoplay: true,
     autoplaySpeed:4000,
+    draggable:false,
     pauseOnFocus:false,
     pauseOnHover:false,
     touchMove:false
