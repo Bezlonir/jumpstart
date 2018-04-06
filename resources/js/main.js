d6f10feb25978e916806dfd4b726d25d94c69873
Bezlonir <michael_bragg99@hotmail.com>
2018-04-05 19:12:28 -0600

    Complete carousel functionality.

---
+++ resources/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.jump-carousel').slick({
+    accessibility:false,
+    autoplay: true,
+    autoplaySpeed:4000,
+    pauseOnFocus:false,
+    pauseOnHover:false,
+    touchMove:false
+  });
+});
